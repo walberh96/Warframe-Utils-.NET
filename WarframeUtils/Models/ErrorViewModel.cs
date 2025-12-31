afd962096974abc17a5b11ceec4e0d9aa3d327cb
Willwitcher <willgamedevelopment@gmail.com>
2025-12-31 14:20:45 -0800

    Created .Net basic template

---
+++ WarframeUtils/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WarframeUtils.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
